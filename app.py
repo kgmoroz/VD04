d1fa6ca630cb186d8618fcda4f315fdd7b101802
kgmoroz <166741223+kgmoroz@users.noreply.github.com>
2024-09-23 00:32:04 +0300

    Task2 added

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     current_time = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
-    return render_template('index.html', current_time=current_time)
+    return render_template('index1.html', current_time=current_time)
 
 if __name__ == "__main__":
     app.run(debug=True)
