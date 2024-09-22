da48090edbb579bb03974583907a811a673c5c76
kgmoroz <166741223+kgmoroz@users.noreply.github.com>
2024-09-23 00:15:56 +0300

    add index.html

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_time = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
     return render_template('index.html', current_time=current_time)
 
 if __name__ == "__main__":
